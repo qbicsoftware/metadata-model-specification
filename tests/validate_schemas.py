f0d8d821be4742852fe75010f8a1997dcf65502a
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2021-07-12 18:46:46 +0200

    folder reorganization

--- tests/validate_schemas.py
+++ tests/validate_schemas.py
@@ -6,18 +6,21 @@ import os
 import json
 
 # Find the schemas
-schema_path = os.path.expanduser(".")
+files_list = list()
+for (dirpath, dirnames, filenames) in os.walk('.'):
+    files_list += [os.path.join(dirpath, file) for file in filenames]
 
 # Perform validation
-for file in os.listdir(schema_path):
+for file in files_list:
     if file.endswith(".json"):
-        print("Validating file: {}".format(file))
+        print(f"Validating file: {file}")
         with open(file, "r") as fh:
             json_schema = json.load(fh)
 
         try:
             jsonschema.Draft7Validator.check_schema(json_schema)
+            print(f"Validation successful for file: {file}")
         except jsonschema.exceptions.SchemaError as e:
             print(f"Invalid Schema {file}, {e}")
 
-print("Validation successful!")
+print("Validation finished!")
