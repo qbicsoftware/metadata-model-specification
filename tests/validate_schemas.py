8a80ecc3848aa794d4aedf8b3175a86941628edc
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2021-07-12 11:59:58 +0200

    update validate_schemas.py

--- tests/validate_schemas.py
+++ tests/validate_schemas.py
@@ -6,17 +6,17 @@ import os
 import json
 
 # Find the schemas
-schema_path = "metadata-test-openbis/schema-test/"
-schemas = os.listdir(schema_path)
+schema_path = [ os.path.expanduser(".") ]
 
 # Perform validation
-for schema in schemas:
-    with open(os.path.join(schema_path, schema), "r") as fh:
-        json_schema = json.load(fh)
+for file in os.listdir(schema_path):
+    if file.endswidth(".json"):
+        with open(file, "r") as fh:
+            json_schema = json.load(fh)
 
-    try:
-        jsonschema.Draft7Validator.check_schema(json_schema)
-    except jsonschema.exceptions.SchemaError as e:
-        print(f"Invalid Schema {schema}, {e}")
+        try:
+            jsonschema.Draft7Validator.check_schema(json_schema)
+        except jsonschema.exceptions.SchemaError as e:
+            print(f"Invalid Schema {file}, {e}")
 
-print("Validation successfull!")
+print("Validation successful!")
