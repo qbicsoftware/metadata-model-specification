052a9b0e3032fba9a7ed3df91d5ecea7aeb6fbf1
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2021-07-13 09:31:51 +0200

    validation fails if schema invalid

--- tests/validate_schemas.py
+++ tests/validate_schemas.py
@@ -1,8 +1,9 @@
 """
 Validate all schemas 
 """
+from _typeshed import IdentityFunction
 import jsonschema
-import os
+import os, sys
 import json
 
 # Find the schemas
@@ -11,6 +12,7 @@ for (dirpath, dirnames, filenames) in os.walk('.'):
     files_list += [os.path.join(dirpath, file) for file in filenames]
 
 # Perform validation
+invalid_schemas = list()
 for file in files_list:
     if file.endswith(".json"):
         print(f"Validating file: {file}")
@@ -22,5 +24,10 @@ for file in files_list:
             print(f"Validation successful for file: {file}")
         except jsonschema.exceptions.SchemaError as e:
             print(f"Invalid Schema {file}, {e}")
-
-print("Validation finished!")
+            invalid_schemas.append(file)
+if len(invalid_schemas):
+    print("Error: the following schemas were invalid:")
+    print(invalid_schemas)
+    sys.exit(1)
+else:
+    print("Validation finished successfully!")
